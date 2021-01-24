699e82819a0d8d5e336cbfe39b704f74df7a3f6f
iamyasinbordbar <yasinbordbar@outlook.com>
2021-01-25 01:06:11 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+var Owlbot = require("owlbot-js");
+
+var client = Owlbot(process.env.YOUR_TOKEN);
+
+bot.on("text", async (ctx) => {
+  try {
+    client.define(ctx.message.text).then(function (result) {
+      result.definitions.map((x, index) => {
+        ctx.reply(
+          (result.definitions.length > 1 ? "Number: " + (index + 1) : "") +
+            "\nWord: " +
+            result.word +
+            "\nPronunciation: " +
+            result.pronunciation +
+            "\n\nType: " +
+            x.type +
+            "\nDefinition: " +
+            x.definition +
+            (x.example !== null ? "\nExample " + x.example : "") +
+            (x.image_url !== null ? "\nImage_url " + x.image_url : "") +
+            (x.emoji !== null ? "\nEmoji " + x.emoji : "")
+        );
+      });
+    });
+  } catch (e) {
+    ctx.reply(e);
+  }
+});
+
+bot.launch();
